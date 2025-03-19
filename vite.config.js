ce7b9335f0cdc3daee75f1e41e3e2b6d70ef72b2
Muhammed ahmed <101899395+muhameedahmed@users.noreply.github.com>
2025-03-19 02:37:44 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -6,4 +6,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-})
+})*/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Ecommerce/', // 👈 Add this line (match your GitHub repo name)
+});
+
